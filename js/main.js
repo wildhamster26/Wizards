102d2ee82ed9a64fa3ed74ec3b034adba8e34054
Maxence Bouret <maxence.bouret@gmail.com>
2018-10-17 17:03:16 +0200

    Add of instructions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Hello Berlin!")
+
+// Iteration 1
+function goToPage(link) {
+  // Goal: hide all the divs with data-page, except the right one
+}
+// Go to main page
+goToPage('home')
+
+// Iteration 2: Listen for click events on <a>
+// Hint: event.preventDefault
