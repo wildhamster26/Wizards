384016f679d5ad20e91b9c34c79db32ccb7e7dc6
Maxence Bouret <maxence.bouret@gmail.com>
2018-10-17 17:59:39 +0200

    Solution

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-console.log("Hello Berlin!")
-
-// Iteration 1
+// When we call goToPage('about'), it will only display the 'about' page
 function goToPage(link) {
-  // Goal: hide all the divs with data-page, except the right one
+  // // Solution 1
+  // $('[data-page]').hide()
+  // $('[data-page='+link+']').show()
+
+  // Solution 2
+  $('[data-page]').each(function() {
+    var page = $(this).data('page')
+    if (page === link)
+      $(this).show()
+    else 
+      $(this).hide()
+  });
+
+  // Add the class active in the navbar
+  $('li.nav-item').each(function(){
+    var href = $(this).find('a.nav-link').attr('href')
+    if (href === link) 
+      $(this).addClass('active')
+    else
+      $(this).removeClass('active')
+  })
 }
 // Go to main page
 goToPage('home')
 
-// Iteration 2: Listen for click events on <a>
-// Hint: event.preventDefault
+// Listen for click events on <a> and redirect to the right page
+$('a').click(function(event){
+  event.preventDefault()
+  var href = $(this).attr('href')
+  goToPage(href)
+})
